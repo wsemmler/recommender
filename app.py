9f393113babe2d5628b85cbe2bdcefcda0dd430d
wsemmler <winasyahputri@gmail.com>
2025-12-05 20:45:38 +0100

    add render port

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ def api_recommend():
     return jsonify(recs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
